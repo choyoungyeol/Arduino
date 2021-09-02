9f2d1ddaa7b0edd9095e1bec2581ed5a7de02b81
Young-Yeol Cho <60500365+choyoungyeol@users.noreply.github.com>
2021-09-02 13:47:36 +0900

    Create Ultrasonic.ino

---
+++ Ultrasonic.ino
@@ -0,0 +1,22 @@
+#define echo 8
+#define trig 9
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+}
+
+void loop() {
+  float cycletime;
+  float distance;
+  digitalWrite(trig, HIGH);
+  delay(10);
+  digitalWrite(trig, LOW);
+  cycletime = pulseIn(echo, HIGH);
+  distance = ((340 * cycletime) / 10000) / 2;
+  Serial.print("Distance:");
+  Serial.print(distance);
+  Serial.println("cm");
+  delay(500);
+}
