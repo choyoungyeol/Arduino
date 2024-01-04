b8a3f02e735c54415a916ca65d7d6b3d23f87330
Young-Yeol Cho <60500365+choyoungyeol@users.noreply.github.com>
2024-01-04 15:41:01 +0900

    Update digital_Shield.ino

--- digital_Shield.ino
+++ digital_Shield.ino
@@ -20,7 +20,7 @@ void loop() {
   int t = dht.readTemperature();
   int Water_value = analogRead(Water_pin);
   int Water = map(Water_value, 1023, 357, 0, 100);
-  Water = constrant(Water, 0, 100);
+  Water = constrain(Water, 0, 100);
   DigitShield.setValue(t);
   delay(2000);
   DigitShield.setValue(h);
